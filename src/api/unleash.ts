761e3cdfed4ecb8242584c9b6418e7f7309df853
Jostein Holje <jostein.holje@nav.no>
2024-08-05 12:58:03 +0200

    unleash: Try/catch rundt initialize
    - ikke dø hvis den feiler lokalt

--- src/api/unleash.ts
+++ src/api/unleash.ts
@@ -1,14 +1,19 @@
 import { Request, Router } from 'express';
 import { initialize, isEnabled } from 'unleash-client';
 import config from '../config';
+import logger from '../logger';
 
-initialize({
-    appName: 'aia-backend',
-    url: config.UNLEASH_SERVER_API_URL,
-    customHeaders: {
-        Authorization: config.UNLEASH_SERVER_API_TOKEN,
-    },
-});
+try {
+    initialize({
+        appName: 'aia-backend',
+        url: config.UNLEASH_SERVER_API_URL || process.env.UNLEASH_SERVER_API_URL!,
+        customHeaders: {
+            Authorization: config.UNLEASH_SERVER_API_TOKEN,
+        },
+    });
+} catch (err) {
+    logger.error({ err, msg: 'Feil i initialize' });
+}
 
 function ensureArray(features?: string | Array<string>) {
     if (!features) {
@@ -28,7 +33,6 @@ type FeatureQuery = {
 
 function unleashRoutes() {
     const router = Router();
-
     router.get('/unleash', (req: Request<any, any, any, FeatureQuery>, res) => {
         const features = ensureArray(req.query.feature).reduce((acc, key) => {
             const k = encodeURIComponent(key);
