242991da1184a43bc4cff79ad7a9904ace95ef3e
Jostein Holje <jostein.holje@nav.no>
2024-11-15 13:28:18 +0100

    Rull tilbake token-validation
    - må finne ut av token-x validering

--- src/middleware/token-validation.ts
+++ src/middleware/token-validation.ts
@@ -1,8 +1,34 @@
 import { Request, RequestHandler } from 'express';
 import { getTokenFromRequest } from '../auth/tokenDings';
 import logger, { getCustomLogProps } from '../logger';
-import { decodeJwt, JWTPayload } from 'jose';
-import { validateIdportenToken, validateTokenxToken } from '@navikt/oasis';
+import {
+    createRemoteJWKSet,
+    decodeJwt,
+    FlattenedJWSInput,
+    JWSHeaderParameters,
+    JWTPayload,
+    jwtVerify,
+    KeyLike,
+} from 'jose';
+import config from '../config';
+
+let tokenxJWKSet: (protectedHeader?: JWSHeaderParameters, token?: FlattenedJWSInput) => Promise<KeyLike>;
+const getTokenXJwkSet = () => {
+    if (!tokenxJWKSet) {
+        tokenxJWKSet = createRemoteJWKSet<KeyLike>(new URL(config.TOKEN_X_JWKS_URI));
+    }
+
+    return tokenxJWKSet;
+};
+
+let idPortenJWKSet: (protectedHeader?: JWSHeaderParameters, token?: FlattenedJWSInput) => Promise<KeyLike>;
+const getIdPortenJwkSet = () => {
+    if (!idPortenJWKSet) {
+        idPortenJWKSet = createRemoteJWKSet<KeyLike>(new URL(config.IDPORTEN_JWKS_URI!));
+    }
+
+    return idPortenJWKSet;
+};
 
 export type AuthLevel = 'Level3' | 'Level4' | 'idporten-loa-substantial' | 'idporten-loa-high';
 export type ValidatedRequest = Request & { user: { level: AuthLevel; ident: string; fnr: string } };
@@ -11,32 +37,33 @@ function isTokenX(decodedToken: JWTPayload) {
     return decodedToken?.iss === process.env.TOKEN_X_ISSUER;
 }
 
-function getValidationResult(token: string, decodedToken: JWTPayload) {
+async function verifyToken(token: string, decodedToken: JWTPayload) {
     if (isTokenX(decodedToken)) {
-        return validateTokenxToken(token);
+        return await jwtVerify(token, getTokenXJwkSet(), {
+            algorithms: ['RS256'],
+        });
     } else {
-        return validateIdportenToken(token);
+        return await jwtVerify(token, getIdPortenJwkSet(), {
+            algorithms: ['RS256'],
+        });
     }
 }
 
 const tokenValidation: RequestHandler = async (req, res, next) => {
     try {
         const token = getTokenFromRequest(req);
-        const decodedToken = decodeJwt(token);
 
-        const validationResult = await getValidationResult(token ?? '', decodedToken);
-
-        if (!validationResult.ok) {
-            logger.warn(validationResult.error);
+        if (!token) {
+            logger.warn(getCustomLogProps(req), 'Bearer token mangler');
             res.sendStatus(401);
             return;
         }
 
-        const payload = validationResult.payload;
-
+        const decodedToken = decodeJwt(token);
+        const result = await verifyToken(token, decodedToken);
         (req as ValidatedRequest).user = {
-            ident: payload.sub!,
-            fnr: payload.pid as string,
+            ident: result.payload.sub!,
+            fnr: result.payload.pid as string,
             level: decodedToken.acr as AuthLevel,
         };
         next();
