03d5a96d2e123e85836358524a71ead63d2e2a31
Jostein Holje <jostein.holje@nav.no>
2024-11-15 13:05:51 +0100

    Håndter token-x i token-validation

--- src/middleware/token-validation.ts
+++ src/middleware/token-validation.ts
@@ -1,16 +1,30 @@
 import { Request, RequestHandler } from 'express';
 import { getTokenFromRequest } from '../auth/tokenDings';
 import logger, { getCustomLogProps } from '../logger';
-import { decodeJwt } from 'jose';
-import { validateIdportenToken } from '@navikt/oasis';
+import { decodeJwt, JWTPayload } from 'jose';
+import { validateIdportenToken, validateTokenxToken } from '@navikt/oasis';
 
 export type AuthLevel = 'Level3' | 'Level4' | 'idporten-loa-substantial' | 'idporten-loa-high';
 export type ValidatedRequest = Request & { user: { level: AuthLevel; ident: string; fnr: string } };
 
+function isTokenX(decodedToken: JWTPayload) {
+    return decodedToken?.iss === process.env.TOKEN_X_ISSUER;
+}
+
+function getValidationResult(token: string, decodedToken: JWTPayload) {
+    if (isTokenX(decodedToken)) {
+        return validateTokenxToken(token);
+    } else {
+        return validateIdportenToken(token);
+    }
+}
+
 const tokenValidation: RequestHandler = async (req, res, next) => {
     try {
         const token = getTokenFromRequest(req);
-        const validationResult = await validateIdportenToken(token);
+        const decodedToken = decodeJwt(token);
+
+        const validationResult = await getValidationResult(token ?? '', decodedToken);
 
         if (!validationResult.ok) {
             logger.warn(validationResult.error);
@@ -18,7 +32,6 @@ const tokenValidation: RequestHandler = async (req, res, next) => {
             return;
         }
 
-        const decodedToken = decodeJwt(token);
         const payload = validationResult.payload;
 
         (req as ValidatedRequest).user = {
